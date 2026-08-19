e4e3115825222418d4f824454ebe340cd22f08d3
Azareth-Tr <azareth289@gmail.com>
2026-08-19 11:12:24 -0600

    FIX: Sidebar y rutas de acceso a las paginas

--- admin/views/salario_promedio/index.php
+++ admin/views/salario_promedio/index.php
@@ -1,2 +1,21 @@
 <div class="flex-grow-1 p-4">
-    <h1>aqui esta el index del salario promedio</h1>
+    <h1>Salario Promedio por Departamento</h1>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Departamento</th>
+                <th>Salario Promedio</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($salarios as $salario): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($salario['departamento']); ?></td>
+                    <td><?php echo htmlspecialchars($salario['salario_promedio']); ?></td>
+                </tr>
+            <?php endforeach; ?>
+            
+        </tbody>
+    </table>
+</div>
--- admin/views/sidebar.php
+++ admin/views/sidebar.php
@@ -1,26 +1,26 @@
 <div class="d-flex vh-100">
-    <div class="d-flex flex-column p-3 bg-dark text-white flex-shrink-0" style="width: 250px;">
+    <div class="d-flex flex-column p-3 bg-dark text-white flex-shrink-0" style="width: 250px; height: 1200px;">
         <ul class="nav nav-pills flex-column mb-auto">
             <li class="nav-item">
-                <a class="nav-link text-white" href="/los-mas-capos/admin/index.php"><h3>Employees</h3></a>
+                <a class="nav-link text-white" href="/admin/index.php"><h3>Employees</h3></a>
                 <h6>Reportes</h6>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/evolucion_contrataciones.php">Evolución de contrataciones</a> 
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/salario_promedio.php">Salario promedio por departamento.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/numero_empleados_departamento.php">Número de empleados por departamento.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/empleado_rango_edad_genero.php">Empleados por rangos de edad y género.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/top_empleados_incremento_salarial.php">Top 10 empleados con mayor incremento salarial en su carrera.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/nueva_consulta.php">Nueva consulta.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/consulta_empleados.php">Consulta de empleados.</a>
+                <a class="nav-link text-white" href="/admin/controllers/evolucion_contrataciones.php">Evolución de contrataciones</a> 
+                <a class="nav-link text-white" href="/admin/controllers/salario_promedio.php">Salario promedio por departamento.</a>
+                <a class="nav-link text-white" href="/admin/controllers/numero_empleados_departamento.php">Número de empleados por departamento.</a>
+                <a class="nav-link text-white" href="/admin/controllers/empleado_rango_edad_genero.php">Empleados por rangos de edad y género.</a>
+                <a class="nav-link text-white" href="/admin/controllers/top_empleados_incremento_salarial.php">Top 10 empleados con mayor incremento salarial en su carrera.</a>
+                <a class="nav-link text-white" href="/admin/controllers/nueva_consulta.php">Nueva consulta.</a>
+                <a class="nav-link text-white" href="/admin/controllers/consulta_empleados.php">Consulta de empleados.</a>
             </li>
             <hr>
             <li class="nav-item">
                 <h6>Gráficos</h6>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/graficos/evolucion_contrataciones_grafico.php">Evolución de contrataciones por año y género.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/graficos/salario_promedio_grafico.php">Salario promedio por departamento.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/graficos/numero_empleados_departamento_grafico.php">Número de empleados por departamento.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/graficos/empleado_rango_edad_genero_grafico.php">Empleados por rangos de edad y género.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/graficos/top_empleados_incremento_salarial_grafico.php">Top 10 empleados con mayor incremento salarial en su carrera.</a>
-                <a class="nav-link text-white" href="/los-mas-capos/admin/controllers/graficos/nueva_consulta_grafico.php">Nueva consulta.</a>
+                <a class="nav-link text-white" href="/admin/controllers/graficos/evolucion_contrataciones_grafico.php">Evolución de contrataciones por año y género.</a>
+                <a class="nav-link text-white" href="/admin/controllers/graficos/salario_promedio_grafico.php">Salario promedio por departamento.</a>
+                <a class="nav-link text-white" href="/admin/controllers/graficos/numero_empleados_departamento_grafico.php">Número de empleados por departamento.</a>
+                <a class="nav-link text-white" href="/admin/controllers/graficos/empleado_rango_edad_genero_grafico.php">Empleados por rangos de edad y género.</a>
+                <a class="nav-link text-white" href="/admin/controllers/graficos/top_empleados_incremento_salarial_grafico.php">Top 10 empleados con mayor incremento salarial en su carrera.</a>
+                <a class="nav-link text-white" href="/admin/controllers/graficos/nueva_consulta_grafico.php">Nueva consulta.</a>
             </li>
             <hr>
             <!-- se puede agregar otra opcion -->
