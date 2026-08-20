37e1aa794ed444ebfbd7818b05e9f8b1abd9cf05
Azareth-Tr <azareth289@gmail.com>
2026-08-19 21:16:30 -0600

    FEAT: vista de nueva consulta

--- admin/controllers/nueva_consulta.php
+++ admin/controllers/nueva_consulta.php
@@ -1,4 +1,8 @@
 <?php
+require_once __DIR__ . "/../models/empleado.php";
 include_once __DIR__ . "/../views/header.php";
+
+$app = new Empleado();
+$empleados = $app->obtenerEmpleadosConTitulosYSalario();
 
 require __DIR__ . "/../views/nueva_consulta/index.php";
