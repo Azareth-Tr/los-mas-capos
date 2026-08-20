448fda39a6715d25de7231d2e6cbf168ce509079
Azareth-Tr <azareth289@gmail.com>
2026-08-19 22:11:13 -0600

    FIX: modificacion de los modelos de nueva consulta y salario promedio por departamento

--- admin/controllers/nueva_consulta.php
+++ admin/controllers/nueva_consulta.php
@@ -1,8 +1,16 @@
 <?php
 require_once __DIR__ . "/../models/empleado.php";
-include_once __DIR__ . "/../views/header.php";
 
 $app = new Empleado();
-$empleados = $app->obtenerEmpleadosConTitulosYSalario();
+$accion = isset($_GET['accion']) ? $_GET['accion'] : null;
 
-require __DIR__ . "/../views/nueva_consulta/index.php";
+include_once __DIR__ . "/../views/header.php";
+
+switch ($accion) {
+    case 'leer':
+    default:
+        $empleados = $app->obtenerEmpleadosConTitulosYSalario();
+        require __DIR__ . "/../views/nueva_consulta/index.php";
+}
+
+include_once __DIR__ . "/../views/footer.php";
