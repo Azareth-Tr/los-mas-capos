0277c62a5c39dfb1dd9574dd7e6bde5249ebea73
Azareth-Tr <azareth289@gmail.com>
2026-08-19 23:55:08 -0600

    FIX: arreglar grafico 3 colores y aliniacion

--- admin/views/graficos/numero_empleados_departamento_grafico/index.php
+++ admin/views/graficos/numero_empleados_departamento_grafico/index.php
@@ -1,7 +1,7 @@
 <div class="flex-grow-1 p-4" style="width: min(1100px, 100%); margin: 0 auto; padding: 24px 16px 40px; text-align: center;">
     <h1>Empleados por Departamento</h1>
     <p>Este gráfico muestra el número de empleados por departamento.</p>
-    <div style="position: relative; width: 100%; height: 560px;">
+    <div style="position: relative; width: 80%; height: 560px; align-items: center; margin: 0 auto;">
         <canvas id="graficoEmpleadosDepartamento"></canvas>
     </div>
 </div>
