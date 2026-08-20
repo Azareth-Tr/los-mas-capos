418cb8c704d2ca5cead9f77af7fbddfab06ff495
DENIsseZea <denissezea04@gmail.com>
2026-08-19 19:02:19 -0600

    Punto cuatro comparación de empleados por edad y genero

--- admin/controllers/empleado_rango_edad_genero.php
+++ admin/controllers/empleado_rango_edad_genero.php
@@ -1,4 +1,45 @@
 <?php
+require_once(__DIR__ . '/../models/empleado_rango_edad_genero.php');
 include_once __DIR__ . "/../views/header.php";
 
-require __DIR__ . "/../views/empleado_rango_edad_genero/index.php";
+
+$app = new Empleado_Rango_Edad_Genero();
+$referenceDate = $_GET['reference_date'] ?? date('Y-m-d');
+$parsedDate = DateTime::createFromFormat('!Y-m-d', $referenceDate);
+if (!$parsedDate || $parsedDate->format('Y-m-d') !== $referenceDate) {
+	$referenceDate = date('Y-m-d');
+}
+
+$gender = $_GET['gender'] ?? '';
+if (!in_array($gender, array('M', 'F'), true)) {
+	$gender = '';
+}
+
+$resultados = $app->read($referenceDate, $gender);
+$rangos = array('<30', '30-39', '40-49', '50-59', '>=60');
+$generos = $gender === '' ? array('M', 'F') : array($gender);
+$totales = array();
+
+foreach ($rangos as $rango) {
+	foreach ($generos as $genero) {
+		$totales[$rango][$genero] = 0;
+	}
+}
+
+foreach ($resultados as $resultado) {
+	$totales[$resultado['age_range']][$resultado['gender']] = (int) $resultado['total_employees'];
+}
+
+$empleados = array();
+foreach ($rangos as $rango) {
+	foreach ($generos as $genero) {
+		$empleados[] = array(
+			'age_range' => $rango,
+			'gender' => $genero,
+			'total_employees' => $totales[$rango][$genero]
+		);
+	}
+}
+
+$totalEmpleados = array_sum(array_column($resultados, 'total_employees'));
+include_once(__DIR__ . "/../views/empleado_rango_edad_genero/index.php");
